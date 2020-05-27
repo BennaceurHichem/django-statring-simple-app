7849e087610e3e9dfc7894d47a98363487cfdffc
BennaceurHichem <gh_bennaceur@esi.dz>
2020-05-27 14:12:19 +0100

    adding vote page

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -5,6 +5,10 @@ app_name='polls'
 urlpatterns=[
     #this mean polls/ will render the polls tempalte 
     path('',views.index,name='index'),
+    #opening the detail page wbased on the question id 
+    path('<int:question_id>/',views.detail,name='detail'),
+    path('<int:question_id>/results',views.results,name='results'),
+    path('<int:question_id>/vote/',views.vote,name='results')
 
 
 
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 from .models import Question,Choice
@@ -7,5 +8,28 @@ from .models import Question,Choice
 
 #Get question and display them 
 def index(request):
-    latest_question_list =Questions.objects.order_by('publish_date')
-    return render(request,'polls/index.html')
+    latest_question_list =Question.objects.order_by('publish_date')[:5]
+    #context is what we want to pas to the template 
+    context =  {'latest_question_list':latest_question_list}
+    #passing cotnext to the tempalte
+    return render(request,'polls/index.html',context)
+
+
+
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question doesn't exist")
+
+    return render(request,'polls/detail.html',{'question':question})
+
+#get question and dispaly result,
+#  we pass quetion as acontext and dispaly result.html
+def results(request,question_id):
+    question= get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+
+
+def vote(request,question_id):
+    
